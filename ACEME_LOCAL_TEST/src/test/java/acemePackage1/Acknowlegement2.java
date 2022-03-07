28f153cec307bd64e3feab346ba603fe6dca9d6f
llmondal <60909016+llmondal@users.noreply.github.com>
2022-03-06 22:24:00 -0500

    test2

---
+++ ACEME_LOCAL_TEST/src/test/java/acemePackage1/Acknowlegement2.java
@@ -0,0 +1,11 @@
+package acemePackage1;
+
+import org.testng.annotations.Test;
+
+public class Acknowlegement2 {
+  @Test
+  public void myTest() {
+	  
+	  System.out.println("This is my test2");
+  }
+}
